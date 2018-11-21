4f989e8e8cacea68cfd14538aa3f5332ff6a091d
oeloem <rosihululum2@gmail.com>
2018-11-21 10:00:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/oeloem/videoonline/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.oeloem.videoonline;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private long ms = 0;
+    private long splashTime = 3000;
+    private boolean splashActive = true;
+    private boolean paused = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        Thread mythread = new Thread(){
+            public void run(){
+                try{
+                    while(splashActive && ms < splashTime){
+                        if (!paused){
+                            ms = ms + 10;
+                            sleep(10);
+                            progressBar.setProgress((int)ms);
+
+                        }
+                    }
+                }catch (Exception e) {
+
+
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        mythread.start();
+    }
+}
